090901b3d53640704e4a58283e5cda4a9aff6052
Andrea Nardelli <andrean@spotify.com>
2019-04-16 17:44:00 +0200

    Added comments for SMBPartitioning

--- src/main/java/smbjoin/beam/SMBPartitioning.java
+++ src/main/java/smbjoin/beam/SMBPartitioning.java
@@ -5,21 +5,26 @@ import java.io.Serializable;
 import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.util.CoderUtils;
 
+/**
+ * @param <JoinKeyT> Type of the join key. Its byte representation is hashed to produce the bucket
+ *     id.
+ * @param <ValueT> Type of the records.
+ */
 public interface SMBPartitioning<JoinKeyT, ValueT> extends Serializable {
   Coder<JoinKeyT> getJoinKeyCoder();
 
+  JoinKeyT getJoinKey(ValueT value);
+
   default byte[] getEncodedJoinKey(ValueT value) {
     try {
-
+      // Encode the key to use .hashBytes later
       return CoderUtils.encodeToByteArray(getJoinKeyCoder(), getJoinKey(value));
     } catch (Exception e) {
-      throw new RuntimeException("bucketHash failed", e);
+      throw new RuntimeException("Hashing failed", e);
     }
   }
 
   default int hashEncodedKey(byte[] encodedJoinKey) {
     return Hashing.murmur3_32().hashBytes(encodedJoinKey).asInt();
   }
-
-  JoinKeyT getJoinKey(ValueT value);
 }
